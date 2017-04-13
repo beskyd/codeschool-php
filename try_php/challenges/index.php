ab21a51eb2ef0b79290a5e790afcdebc7cdb8adc
Ay Beskyd <beskydmail@gmail.com>
2017-04-13 21:10:51 +0000

    l3s1: edit index.php to utilize if/else conditional statement

--- try_php/challenges/index.php
+++ try_php/challenges/index.php
@@ -1,34 +1,33 @@
 <?php
+$year = 1774;
 $cities = array(
   'tokyo' => [
     13.6,
     1868,
-    'Japan'
-    ],
+    'Japan',
+  ],
   'dc' => [
     0.6,
     1790,
     'United States'
-    ],
+  ],
   'moscow' => [
     11.5,
     1147,
     'Russia'
-    ]
+  ],
+  'london' => [
+    8.6,
+    43,
+    'England'
+  ],
 );
-
-$cities['london'] = [
-  8.6,
-  43,
-  'England'
-];
-
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/master.css" media="screen">
+    <link rel="stylesheet" href="/css/master.css" media="screen" title="no title">
     <title>Try PHP</title>
   </head>
   <body>
@@ -36,21 +35,29 @@ $cities['london'] = [
       <div class="sidebar">
         <div class="info">
           <ul>
-            <li>City of London</li>
-            <li>Population: <?php echo $cities['london'][0]; ?> million.</li>
-            <li>Established: <?php echo $cities['london'][1]; ?> AD.</li>
-            <li>Country: <?php echo $cities['london'][2]; ?></li>
+            <li>City of Washington DC</li>
+            <li>Population: <?php echo $cities['dc'][0]; ?> million.</li>
+            <li>Established: <?php echo $cities['dc'][1]; ?> AD.</li>
+            <li>Country: <?php echo $cities['dc'][2]; ?></li>
           </ul>
         </div>
         <div class="details">
           <p>
-            London is the capital and most populous city of England,
-            Great Britain, and the United Kingdom.
-            On the River Thames in the south east of the island of Great Britain,
-            London has been a major settlement for two millennia.
-            It was founded by the Romans, who named it Londinium.
+            <?php
+              if ($year >= $cities['dc'][1]) {
+                echo 'Washington DC is the capital of the United States';
+              } elseif ($year == 1774) {
+                echo 'The US capital is still in Philadelphia.';
+              } else {
+                $difference = $cities['dc'][1] - $year;
+                echo "There are $difference years until DC is established.";
+              }
+            ?>
           </p>
         </div>
+      </div>
+      <div class="map">
+        <img src="/images/map-us.png" alt="map of the United States" />
       </div>
     </div>
   </body>
