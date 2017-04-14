744c6ec1b0973d01b1996d5d810bf37bc12c42e6
Ay Beskyd <beskydmail@gmail.com>
2017-04-14 14:24:12 +0000

    l2s2: remove all form code from index1.php and place it to content.php

---
+++ close_encounters_with_php/challenges/content.php
@@ -0,0 +1,18 @@
+<form class="form">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name">
+    </div>
+    
+    <div class="form-group">
+      <label for="date">Date of Departure</label>
+      <input type="date" class="form-control" id="date">
+    </div>
+    
+    <div class="form-group">
+      <label for="reason">Reason for Visit</label>
+      <input type="text" class="form-control" id="reason">
+    </div>
+    
+    <button type="submit" class="btn" disabled="disabled">Submit</button>
+</form>
