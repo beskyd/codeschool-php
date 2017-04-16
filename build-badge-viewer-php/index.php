cc405e0320f5b133a6c17ade07dc6f0378b77135
Ay Beskyd <beskydmail@gmail.com>
2017-04-16 12:10:28 +0000

    grab user profile json data and display username in the page title

--- build-badge-viewer-php/index.php
+++ build-badge-viewer-php/index.php
@@ -1,9 +1,16 @@
-<?php phpinfo(); ?>
+<?php 
+//phpinfo(); 
+$data = file_get_contents('https://www.codeschool.com/users/AyBeskyd.json');
+// use json_decode with the second parameter set to -true- for the assoc. array option instead of the default object
+$json_data = json_decode($data, true);
+//var_dump($json_data['courses']['completed']);
+$courses = $json_data['courses']['completed'];
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Badge Viewer with PHP</title>
+    <title><?php echo $json_data['user']['username']; ?>'s badges</title>
   </head>
   <body>
     
