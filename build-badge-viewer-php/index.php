be69262a623ed7ad505d732c2d8a8a9ff8d57585
Ay Beskyd <beskydmail@gmail.com>
2017-04-16 12:57:52 +0000

    add styles with grid.css

--- build-badge-viewer-php/index.php
+++ build-badge-viewer-php/index.php
@@ -11,6 +11,8 @@ $courses = $json_data['courses']['completed'];
   <head>
     <meta charset="utf-8">
     <title><?php echo $json_data['user']['username']; ?>'s badges</title>
+    <link href="https://fonts.googleapis.com/css?family=Coda" rel="stylesheet">
+    <link rel="stylesheet" href="/css/grid.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
     <?php 
