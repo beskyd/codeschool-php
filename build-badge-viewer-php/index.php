ee08bed9062d2214e9ce02ee3e80aa371b595103
Ay Beskyd <beskydmail@gmail.com>
2017-04-16 11:23:23 +0000

    add basic html code to index.php

---
+++ build-badge-viewer-php/index.php
@@ -0,0 +1,11 @@
+<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Badge Viewer with PHP</title>
+  </head>
+  <body>
+    
+  </body>
+</html>
