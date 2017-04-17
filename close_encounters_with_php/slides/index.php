cc03acd6ba84c9f16cc9e04132cff8b7ca2e8045
Ay Beskyd <beskydmail@gmail.com>
2017-04-17 20:29:47 +0000

    add index.php with a simple form for sightseeing submitting

---
+++ close_encounters_with_php/slides/index.php
@@ -0,0 +1,17 @@
+<form class="" action="index.php" method="post">
+    
+    <label for="date">Date of Sighting</label><br>
+    <input type="text" name="date" value=""/>
+    <hr>
+    
+    <label for="email">Your Email</label><br>
+    <input type="text" name="email" value=""/>
+    <hr>
+    
+    <label for="desc">Describe the Sighting</label><br>
+    <textarea name="desc" rows="8" cols="40"></textarea>
+    <br>
+    
+    <input type="submit" value="Submit"/>
+    
+</form>
