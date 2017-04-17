bb6d4d0d6d24a236b66b2c7c45740b9cf066e39c
Ay Beskyd <beskydmail@gmail.com>
2017-04-17 20:34:32 +0000

    echo POST data

--- close_encounters_with_php/slides/index.php
+++ close_encounters_with_php/slides/index.php
@@ -1,17 +1,40 @@
-<form class="" action="index.php" method="post">
+<?php 
+//Echo each item in POST
+echo $_POST['date'];
+echo $_POST['email'];
+echo $_POST['desc'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="" type="text/css" />
+    </head>
+    <body>
+        
+        <form class="" action="index.php" method="post">
     
-    <label for="date">Date of Sighting</label><br>
-    <input type="text" name="date" value=""/>
-    <hr>
-    
-    <label for="email">Your Email</label><br>
-    <input type="text" name="email" value=""/>
-    <hr>
-    
-    <label for="desc">Describe the Sighting</label><br>
-    <textarea name="desc" rows="8" cols="40"></textarea>
-    <br>
-    
-    <input type="submit" value="Submit"/>
-    
-</form>
+            <label for="date">Date of Sighting</label><br>
+            <input type="text" name="date" value=""/>
+            <hr>
+            
+            <label for="email">Your Email</label><br>
+            <input type="text" name="email" value=""/>
+            <hr>
+            
+            <label for="desc">Describe the Sighting</label><br>
+            <textarea name="desc" rows="8" cols="40"></textarea>
+            <br>
+            
+            <input type="submit" value="Submit"/>
+            
+        </form>
+        
+    </body>
+</html>
+
+
+
