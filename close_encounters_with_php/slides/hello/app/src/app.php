1b4c2825e7312b0ff25e694c7a1778e0480bf3ec
Ay Beskyd <beskydmail@gmail.com>
2017-04-18 21:03:53 +0000

    remove whitespaces on form data with trim

--- close_encounters_with_php/slides/hello/app/src/app.php
+++ close_encounters_with_php/slides/hello/app/src/app.php
@@ -1,9 +1,9 @@
 <?php 
 //check if the server request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $date = $_POST['date'];
-    $email = $_POST['email'];
-    $description = $_POST['description'];
+    $date = trim($_POST['date']);
+    $email = trim($_POST['email']);
+    $description = trim($_POST['description']);
     //Echo each item in POST
     if (!empty($date) && !empty($email) && !empty($description)) {
         echo "<p>Date: $date</p>";
