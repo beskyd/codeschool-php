fb5026860b2395ee8f2e40c896c5a4e2fd49961c
Ay Beskyd <beskydmail@gmail.com>
2017-04-18 21:12:40 +0000

    add  validation against filFILTER_VALIDATE_EMAIL with filter_var

--- close_encounters_with_php/slides/hello/app/src/app.php
+++ close_encounters_with_php/slides/hello/app/src/app.php
@@ -7,7 +7,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //Echo each item in POST
     if (!empty($date) && !empty($email) && !empty($description)) {
         echo "<p>Date: $date</p>";
-        echo "<p>Email: $email</p>";
+        //filter_var checks a variable against a filter and returns TRUE if it passes
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "<p>Email: $email</p>";
+        }
         echo '<p>' . htmlspecialchars($description) . '</p>';
     }
 }
