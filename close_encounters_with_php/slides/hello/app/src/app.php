e16c5a029409d59193b7baeaed9d7a3eef7793ea
Ay Beskyd <beskydmail@gmail.com>
2017-04-18 21:24:35 +0000

    move  validation code to a custom validation function

--- close_encounters_with_php/slides/hello/app/src/app.php
+++ close_encounters_with_php/slides/hello/app/src/app.php
@@ -1,4 +1,15 @@
 <?php 
+function validate_date($date_string) 
+{
+    //strtotime will convert most any date to a Unix timestamp
+    if ($time = strtotime($date)) {
+        return date('F jS Y', $time);
+    } else {
+        return $date_string . ' does not look valid';
+    }
+        
+}
+
 //check if the server request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $date = trim($_POST['date']);
@@ -6,10 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $description = trim($_POST['description']);
     //Echo each item in POST
     if (!empty($date) && !empty($email) && !empty($description)) {
-        //strtotime will convert most any date to a Unix timestamp
-        if ($time = strtotime($date)) {
-            echo "<p>Date: date('F jS Y', $time)</p>";
-        }
+        echo validate_date($date);
         //filter_var checks a variable against a filter and returns TRUE if it passes
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             echo "<p>Email: $email</p>";
