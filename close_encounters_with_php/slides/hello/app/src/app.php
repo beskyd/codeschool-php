df1de955b9bf47c7ded1be4997cef2193be1d158
Ay Beskyd <beskydmail@gmail.com>
2017-04-19 20:43:09 +0000

    move the code of validate_date function to validation.php to comply with  PSR-1

--- close_encounters_with_php/slides/hello/app/src/app.php
+++ close_encounters_with_php/slides/hello/app/src/app.php
@@ -1,14 +1,5 @@
 <?php 
-function validate_date($date_string) 
-{
-    //strtotime will convert most any date to a Unix timestamp
-    if ($time = strtotime($date)) {
-        return date('F jS Y', $time);
-    } else {
-        return $date_string . ' does not look valid';
-    }
-        
-}
+require __DIR__ . '/validation.php';
 
 //check if the server request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
---
+++ close_encounters_with_php/slides/hello/app/src/validation.php
@@ -0,0 +1,11 @@
+<?php
+function validate_date($date_string) 
+{
+    //strtotime will convert most any date to a Unix timestamp
+    if ($time = strtotime($date)) {
+        return date('F jS Y', $time);
+    } else {
+        return $date_string . ' does not look valid';
+    }
+        
+}
