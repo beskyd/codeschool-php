2da9bae0c5d0bbd1bb2f4a1eea6bf9654d3556ab
Ay Beskyd <beskydmail@gmail.com>
2017-04-20 20:22:42 +0000

    use Validation class and custom validator

--- close_encounters_with_php/slides/hello/app/src/validation.php
+++ close_encounters_with_php/slides/hello/app/src/validation.php
@@ -2,14 +2,17 @@
 use Respect\Validation\Validator;
 
 $v = new Validator;
+//var_dump($v);
 
 function validate_date($date_string) 
 {
-    //strtotime will convert most any date to a Unix timestamp
-    if ($time = strtotime($date)) {
-        return date('F jS Y', $time);
+    $date_validator = Validator::date('d-m-Y')->notEmpty();
+    if ($date_validator->validate($date_string)) {
+        //strtotime will convert most any date to a Unix timestamp
+        $date_time = strtotime($date_string);
+        return date('F jS Y', $date_time);
     } else {
-        return $date_string . ' does not look valid';
+        return 'The date must be in a DD-MM-YYYY format.';
     }
-        
+    
 }
