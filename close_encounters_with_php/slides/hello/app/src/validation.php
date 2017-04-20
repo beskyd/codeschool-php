2ae352dffbaee26efbf1c98bd2595c83a1b2c162
Ay Beskyd <beskydmail@gmail.com>
2017-04-20 20:29:15 +0000

    use try/catch block for the date validation

--- close_encounters_with_php/slides/hello/app/src/validation.php
+++ close_encounters_with_php/slides/hello/app/src/validation.php
@@ -1,5 +1,7 @@
 <?php
 use Respect\Validation\Validator;
+use Respect\Validation\Exceptions\NestedValidationException;
+
 
 $v = new Validator;
 //var_dump($v);
@@ -7,12 +9,16 @@ $v = new Validator;
 function validate_date($date_string) 
 {
     $date_validator = Validator::date('d-m-Y')->notEmpty();
-    if ($date_validator->validate($date_string)) {
+    try:
+        $date_validator->assert($date_string);
         //strtotime will convert most any date to a Unix timestamp
         $date_time = strtotime($date_string);
         return date('F jS Y', $date_time);
-    } else {
-        return 'The date must be in a DD-MM-YYYY format.';
+    } catch (NestedValidationException $e) {
+        //The NestedValidationException has a method named getMessages that will return some useful information
+        //will return the array of errors
+        var_dump($e->getMessages());
+        //return 'The date must be in a DD-MM-YYYY format.';
     }
     
 }
